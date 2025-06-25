ce47e9d8c346d5dc265579fa32d5b8d1ebb48f8b
Lucas-hrl <lucas.herchuel@uha.fr>
2025-06-25 14:22:40 +0200

    Delete myapp directory

--- myapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Capteur
-
-
-class CapteurForm(forms.ModelForm):
-    class Meta:
-        model = Capteur
-        fields = ['nom_capteur', 'emplacement']
